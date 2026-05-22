ce0263902f12b2dac0ab4551ea713bb087637542
yakusu123 <vic3nt3.ord3n3s@gmail.com>
2026-05-22 18:02:24 -0400

    Feat: add the code imputar.py

---
+++ src/imputar.py
@@ -0,0 +1,12 @@
+import polars as pl  # type: ignore[import-untyped]
+import os
+
+df = pl.read_csv("data/interim/validado.csv")
+
+dfI = df.with_columns(
+    pl.col("nota1").fill_null(pl.col("nota1").median()),
+    pl.col("nota2").fill_null(pl.col("nota2").median()),
+    pl.col("nota3").fill_null(pl.col("nota3").median()),
+    df.with_columns(pl.col("asistencia").fill_null(pl.col("asistencia")).mean()))
+
+dfI.write_csv("data/interim/imputado.csv")
